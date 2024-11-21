2f6470c091c4bcfe61d96c7e81e786d00bf8b35b
maksimkins <maks.aleshkov.04@gmail.com>
2024-11-21 21:02:09 +0400

    Start Project

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,11 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
+
+const defaultConfig = getDefaultConfig(__dirname);
+const { assetExts, sourceExts } = defaultConfig.resolver;
+
+const {
+  wrapWithReanimatedMetroConfig,
+} = require('react-native-reanimated/metro-config');
 
 /**
  * Metro configuration
@@ -6,6 +13,16 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  transformer: {
+    babelTransformerPath: require.resolve(
+      "react-native-svg-transformer/react-native"
+    )
+  },
+  resolver: {
+    assetExts: assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...sourceExts, "svg"]
+  }
+};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = mergeConfig(defaultConfig, wrapWithReanimatedMetroConfig(config));
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+import { Root } from "./navigation/Root";
+
+export const App = () => {
+  return (
+    <View>
+        <Root/>
+    </View>
+  )
+}
